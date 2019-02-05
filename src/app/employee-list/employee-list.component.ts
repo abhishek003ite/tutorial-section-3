3779c0eff67e3a8c801fd6ac95f173790ecafc38
Abhishek Shrivastava <abhishek.shrivastava1987@gmail.com>
2019-02-05 17:17:52 +0530

    HTTPError Section 3 Ep 22

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -14,12 +14,14 @@ import { EmployeeService } from '../employee.service';
 export class EmployeeListComponent implements OnInit {
 
   public employees = [];
+  public errorMsg;
 
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this._employeeService.getEmployees()
-        .subscribe(data => this.employees = data);
+        .subscribe(data => this.employees = data,
+                   error => this.errorMsg = error);
   }
 
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IEmployee } from './employee';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,11 @@ export class EmployeeService {
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<IEmployee[]> {
-    return this.http.get<IEmployee[]>(this._url);
+    return this.http.get<IEmployee[]>(this._url)
+                    .catch(this.errorHandler);
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    return Observable.throw(error.message || "Server Error");
   }
 }
